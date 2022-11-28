e6b0f1089d1890a6789f3df6875b70b46bb49f74
RichardOkongo <rirachard19@gmail.com>
2022-11-28 19:50:47 +0300

    made changes to 2 files

--- Speeddetector/Speeddetector.js
+++ Speeddetector/Speeddetector.js
@@ -1,4 +1,5 @@
 // Program that inputs speed of a car
+// For every 5kms above speed limit give the driver one demerit point
 // if speed is 80 print 2 points
 // more than 12 points license suspended
 let speed;
@@ -12,6 +13,7 @@ function speedDetector(speed){
         }else{
             return ("Demerit: " + velocity)
         }
+        // Print number of demerit points
     }
 }
-console.log(speedDetector(80))
+console.log(speedDetector(75))
